602a4ab66bc41c96fc0554bb30e5ad09208dfa03
Parshav Chauhan <pchauhan@clientresourcesinc.com>
2019-02-19 19:08:54 -0600

    Minor test

--- Encryption/test/se/simbio/encryption/EncrpytionKTest.kt
+++ Encryption/test/se/simbio/encryption/EncrpytionKTest.kt
@@ -1,21 +1,19 @@
 package se.simbio.encryption
 
+import org.junit.Assert.*
 import org.junit.Test
-import kotlin.test.assertEquals
-import kotlin.test.assertNotNull
-import kotlin.test.assertNull
 
 class EncrpytionKTest {
 
     companion object {
         private const val DEFAULT_KEY = "default_key"
         private const val DEFAULT_SALT = "default_salt"
-        private val DEFAULT_IV = byteArrayOf(0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0)
+        private val DEFAULT_IV = byteArrayOf(0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0)
         private const val textToEncrypt = "Sample Kt Text to encrypt"
     }
 
     @Test
-    fun `Basic callback test`() {
+    fun `Basic test`() {
         val encryption = EncryptionK.getDefault(DEFAULT_KEY, DEFAULT_SALT, DEFAULT_IV)
         val encryptedText = encryption.encrypt(textToEncrypt)
         assertNotNull(encryptedText)
@@ -25,6 +23,15 @@ class EncrpytionKTest {
             assertNull(e)
             assertNotNull(s)
             assertEquals(s, textToEncrypt)
+        }
+    }
+
+    @Test
+    fun `Encrypt Callback test`() {
+        val encryption = EncryptionK.getDefault(DEFAULT_KEY, DEFAULT_SALT, DEFAULT_IV)
+        val encryptedText = encryption.encrypt(textToEncrypt) { s, e ->
+            assertNull(e)
+            assertNotNull(s)
         }
     }
 }
